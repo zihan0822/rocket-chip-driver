c9ec3d29ae073d2e28f13f9d9efa06400316626f
Zihan <zihanli0822@gmail.com>
2025-10-17 10:00:01 -0400

    feat(jit): allow recursively inlining walk
    we also turn off `inline` feature for driver, because the inlining
    everything policy currently makes driver unreasonably slow
    
    Signed-off-by: Zihan <zihanli0822@gmail.com>

--- patronus/src/sim/jit/inliner.rs
+++ patronus/src/sim/jit/inliner.rs
@@ -1,7 +1,8 @@
 use cranelift::codegen::inline::{Inline, InlineCommand};
 use cranelift::codegen::ir::{self, entities, function};
 
-type CalleeQueryServer<'a> = &'a dyn Fn(entities::FuncRef) -> Option<&'a function::Function>;
+type CalleeQueryServer<'a> =
+    &'a dyn Fn(entities::FuncRef, &function::Function) -> Option<&'a function::Function>;
 
 pub(super) struct JITInliner<'a> {
     callee_query_server: CalleeQueryServer<'a>,
@@ -18,13 +19,13 @@ impl<'a> JITInliner<'a> {
 impl Inline for JITInliner<'_> {
     fn inline(
         &mut self,
-        _caller: &function::Function,
+        caller: &function::Function,
         _call_inst: entities::Inst,
         _call_opcode: ir::Opcode,
         callee: entities::FuncRef,
         _call_args: &[entities::Value],
     ) -> InlineCommand<'_> {
-        if let Some(callee) = (self.callee_query_server)(callee) {
+        if let Some(callee) = (self.callee_query_server)(callee, caller) {
             InlineCommand::Inline {
                 callee: std::borrow::Cow::Borrowed(callee),
                 visit_callee: true,
