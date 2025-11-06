4f3832c24c2cd3594df4bc06eabcdb73d1508774
Zihan <zihanli0822@gmail.com>
2025-11-06 17:51:10 -0500

    chore: update verilog2btor yosys script
    get rid of a few optimizations that have little help on the final
    simulation peformance for quicker synthesis
    
    Signed-off-by: Zihan <zihanli0822@gmail.com>

--- verilog2btor.tcl
+++ verilog2btor.tcl
@@ -2,15 +2,14 @@ foreach file [split $::env(VERILOG_FILES)] {
     yosys read_verilog -sv $file
 }
 yosys hierarchy -top $::env(TOP_MODULE) -check
-yosys prep -top $::env(TOP_MODULE) 
+yosys proc
+yosys flatten
+yosys opt_clean
+yosys wreduce -keepdc 
 yosys memory_dff
 yosys memory_collect
-yosys flatten
+yosys opt_clean
 yosys memory_nordff
-yosys memory_unpack
-yosys splitnets -driver
-yosys setundef -undriven -init -zero
-yosys opt
 yosys async2sync
 yosys dffunmap
 yosys write_btor -s $::env(BTOR_OUTPUT) 
